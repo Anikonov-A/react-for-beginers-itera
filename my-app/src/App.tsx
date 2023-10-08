1336bb8e9d5cd51bd3e3c2e1c643d21b2b3f8cce
Anton Anikonov <antonanikonov1@gmail.com>
2023-10-08 21:32:54 +0300

    first

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css'
+import './Header.css'
+import {Header} from "./HeaderProps";
+import {Main} from "./Main";
+
+
+const App = () => (
+    <div className="App">
+        <Header text="!!!!"/>
+        <Main />
+    </div>
+)
+
+
+export default App;
